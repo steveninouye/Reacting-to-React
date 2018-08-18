17db46b9e38eeea3ee805b0c19d4c77c7b7d4a2c
Steven Inouye <steveninouye@msn.com>
2018-08-18 08:16:02 -1000

    added Fragment into JSX

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const App = (props) => {
-    return <h1>Hello</h1>;
-};
+// const App = (props) => {
+//     return <h1>{props.str}</h1>;
+// };
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'this is text from sate'
+        };
+    }
+
+    render() {
+        return(
+        <Fragment>
+            <h1>{`${this.props.str} ${this.state.text}`}</h1>
+            <input type="text" />
+        </Fragment>)
+    }
+}
 
 export default App;
