a77e3b036fd6f64824a9238c7c51343272fc62ff
Steven Inouye <steveninouye@msn.com>
2018-08-18 16:29:34 -1000

    got setState to work with a function.  Found that event was being garbage collected and needed to be stored in a variable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,33 @@
 import React, { Component, Fragment } from 'react';
 
-// const App = (props) => {
-//     return <h1>{props.str}</h1>;
-// };
-
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             text: 'this is text from sate'
         };
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(event) {
+        const value = event.target.value;
+        this.setState((prevState) => {
+            console.log(value);
+            return { text: value };
+        });
     }
 
     render() {
-        return(
-        <Fragment>
-            <h1>{`${this.props.str} ${this.state.text}`}</h1>
-            <input type="text" />
-        </Fragment>)
+        return (
+            <Fragment>
+                <h1>{`${this.props.str} ${this.state.text}`}</h1>
+                <input
+                    type="text"
+                    value={this.state.text}
+                    onChange={this.handleInput}
+                />
+            </Fragment>
+        );
     }
 }
 
